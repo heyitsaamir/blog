31a13a81b4b90754e6619a41cd0125ebf144cfbc
Aamir Jawaid <ajawaid191@gmail.com>
2022-12-28 10:23:30 -0800

    center header

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 const Intro = () => {
   return (
     <section className="flex-row items-center md:justify-between pt-16 pb-16 pd:pb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight pd:pr-8 text-end">
+      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight pd:pr-8 text-center">
         aamir j.
       </h1>
     </section>
