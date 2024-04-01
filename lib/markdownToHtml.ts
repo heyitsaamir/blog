40840b373a7fc6a052e82ab2db904b159f96e534
Aamir Jawaid <ajawaid191gmail.com>
2024-04-01 08:25:01 -0700

    Fix image issue

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,12 +3,16 @@ import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeMermaid from "rehype-mermaid";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeRaw from "rehype-raw";
 import { unified } from "unified";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeSanitize)
     .use(rehypeMermaid, { strategy: "pre-mermaid" })
     .use(rehypeFormat)
     .use(rehypeStringify)
