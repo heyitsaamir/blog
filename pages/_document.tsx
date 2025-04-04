33858c897e9b5f4c0077ad9c73a1debe952a2511
Aamir Jawaid <ajawaid191gmail.com>
2025-04-04 08:09:43 -0700

    Fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script id="clarity-script">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qyxlvb5uz3");`}
+        </Script>
+      </Head>
       <body className="bg-stone-50  dark:bg-stone-800 dark:text-stone-100">
         <Main />
         <NextScript />
