a01219f1b73c114abc4aecc977a3a0654a30f1b1
heyitsaamir <ajawaid191@gmail.com>
2025-05-30 08:57:44 -0700

    Add intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,12 @@ export default function Index({ posts }: Props) {
       <Layout head={<title>aamir j. blog</title>} isIndexPage>
         <section className="md:mx-48 flex flex-col gap-12 py-8">
           <div>
+            <p className="mb-12">
+              <em>
+                Hi! I'm Aamir. I'm a software engineer. I love tinkering with code and trying out new technologies to build useful (and useless) things.
+                I currently work for Microsoft. In this blog, I talk about stuff I'm learning, things I find interesting, and sometimes just random thoughts.
+              </em>
+            </p>
             <ul className="flex flex-col gap-6">
               {posts.map((post) => (
                 <li key={post.slug} className="flex flex-col">
