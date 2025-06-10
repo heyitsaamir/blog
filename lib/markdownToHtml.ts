4555442f948fd4d7487aae12317e84b74bb5b913
Aamir <48929123+heyitsaamir@users.noreply.github.com>
2025-06-09 22:26:25 -0700

    Update markdownToHtml.ts

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,7 @@
 import rehypeFormat from "rehype-format";
 import rehypeMermaid from "rehype-mermaid";
 import rehypeRaw from "rehype-raw";
-import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
+// import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
@@ -15,13 +15,13 @@ export default async function markdownToHtml(markdown: string, theme?: 'dark' | 
     .use(remarkGfm)
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
-    .use(rehypeSanitize, {
-      ...defaultSchema,
-      attributes: {
-        ...defaultSchema.attributes,
-        "*": ["className"], // To allow mermaid classes to continue to work
-      },
-    })
+    //.use(rehypeSanitize, {
+    //  ...defaultSchema,
+    //  attributes: {
+    //    ...defaultSchema.attributes,
+    //    "*": ["className"], // To allow mermaid classes to continue to work
+    //  },
+    //})
     .use(rehypeMermaid, {
       strategy: "pre-mermaid"
     })
