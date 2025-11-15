49a2b1486acfbe2d3d5d4dbdf81ea92014d90379
Aamir Jawaid <48929123+heyitsaamir@users.noreply.github.com>
2025-11-15 12:32:17 -0800

    Update underline offset in header link

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
 	return (
 		<h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pt-8 text-stone-900 dark:text-stone-100">
-			<Link href="/" className="hover:underline underline-offset-[2.3px]">
+			<Link href="/" className="hover:underline underline-offset-[3px]">
 				aamir j
 			</Link>
 			.
