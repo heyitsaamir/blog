f55e1c205fe6b1c62dbe66611fe3295610bbd3ed
Aamir Jawaid <ajawaid191gmail.com>
2025-11-14 22:20:58 -0800

    Fix up

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
 const Header = () => {
-  return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pt-8 text-stone-900 dark:text-stone-100">
-      <Link href="/" className="hover:underline underline-offset-3">
-        aamir j
-      </Link>
-      .
-    </h2>
-  );
+	return (
+		<h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pt-8 text-stone-900 dark:text-stone-100">
+			<Link href="/" className="hover:underline underline-offset-[2.3px]">
+				aamir j
+			</Link>
+			.
+		</h2>
+	);
 };
 
 export default Header;
